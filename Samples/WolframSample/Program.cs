1dbab2be21e31b200512a20d49c700217f2b1104
Manuel-S <manu-lg@hotmail.de>
2014-07-24 15:06:16 +0200

    Most basic resource management

--- Samples/WolframSample/Program.cs
+++ Samples/WolframSample/Program.cs
@@ -1,67 +1,1 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using Scrappy;
-
-namespace WolframSample
-{
-	class Program
-	{
-		private static void Main(string[] args)
-		{
-			// the browser holds cookies and stuff for us
-			var browser = new Browser();
-
-			//navigate to the landing page
-			var startpage = browser.Open("http://www.wolframalpha.com/").Result;
-
-
-			//submit our query using the Form helper
-			var query = string.Join(" ", args);
-			var resultpage = startpage.GetForm("#calculate").Set("i", query).Submit().Result;
-
-
-			//get all <script>-Contents (the plain text result is hidden in the javascript)
-			var page = resultpage.Select("script").SelectionHtml(true);
-
-
-			//get the Box in which the result is displayed, if there is a result
-			var resultindex = page.IndexOf(@"'\x22Result\x22'", StringComparison.InvariantCulture);
-			if (resultindex <= 0)
-			{
-				//try to find a decimal approximation if there is no exact result available
-				resultindex = page.IndexOf(@"'\x22DecimalApproximation\x22'", StringComparison.InvariantCulture);
-			}
-			var podindex = page.IndexOf("pod_", resultindex - 170, StringComparison.InvariantCulture);
-			var podstr = page.Substring(podindex, 8);
-
-
-			// on complex queries, the computation is done asynchronously and needs to be loaded seperately
-			var isAsync = new Regex("asynchronousPod\\(\"(?<result>.*?)\", \".*?\", \".*?\","
-					+ " \".*?\", \".*?\", \"Result\" \\)").Match(page);
-			if (isAsync.Success)
-			{
-				var asyncResult = resultpage.Open(isAsync.Groups["result"].Value).Result;
-
-				//replace the content we are going to look for the result with the asynchronously gathered content
-				page = asyncResult.Render();
-			}
-
-
-			// try to get the result string(s)
-			var result = new Regex(podstr + ".*?stringified\": \"(?<result>.*?)\"")
-				.Matches(page);
-
-
-			foreach (Match match in result)
-			{
-				Console.WriteLine("Result:");
-				Console.WriteLine(match.Groups["result"].Value.Replace("\\/", "/"));
-			}
-
-			if(result.Count == 0)
-			{
-				Console.WriteLine("No result found");
-			}
-		}
-	}
-}
+﻿using System;using System.Text.RegularExpressions;using Scrappy;namespace WolframSample{	class Program	{		private static void Main(string[] args)		{			// the browser holds cookies and stuff for us			var browser = new Browser();			//navigate to the landing page			var startpage = browser.Open("http://www.wolframalpha.com/").Result;			//submit our query using the Form helper			var query = string.Join(" ", args);			var resultpage = startpage.GetForm("#calculate").Set("i", query).Submit().Result;						//get all <script>-Contents (the plain text result is hidden in the javascript)			var page = resultpage.Select("script").SelectionHtml(true);			//get the Box in which the result is displayed, if there is a result			var resultindex = page.IndexOf(@"'\x22Result\x22'", StringComparison.InvariantCulture);			if (resultindex <= 0)			{				//try to find a decimal approximation if there is no exact result available				resultindex = page.IndexOf(@"'\x22DecimalApproximation\x22'", StringComparison.InvariantCulture);			}			var podindex = page.IndexOf("pod_", resultindex - 170, StringComparison.InvariantCulture);			var podstr = page.Substring(podindex, 8);			// on complex queries, the computation is done asynchronously and needs to be loaded seperately			var isAsync = new Regex("asynchronousPod\\(\"(?<result>.*?)\", \".*?\", \".*?\","					+ " \".*?\", \".*?\", \"Result\" \\)").Match(page);			if (isAsync.Success)			{				var asyncResult = resultpage.Open(isAsync.Groups["result"].Value).Result;				//replace the content we are going to look for the result with the asynchronously gathered content				page = asyncResult.Render();			}			// try to get the result string(s)			var result = new Regex(podstr + ".*?stringified\": \"(?<result>.*?)\"")				.Matches(page);			foreach (Match match in result)			{				Console.WriteLine("Result:");				Console.WriteLine(match.Groups["result"].Value.Replace("\\/", "/"));			}			if(result.Count == 0)			{				Console.WriteLine("No result found");			}		}	}}
--- Scrappy/Form.cs
+++ Scrappy/Form.cs
@@ -1,88 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CsQuery;
-using CsQuery.ExtensionMethods;
-using CsQuery.ExtensionMethods.Internal;
-
-namespace Scrappy
-{
-	public class Form
-	{
-		private readonly Browser browser;
-		private readonly CQ formNode;
-		private readonly Uri baseUri;
-		private readonly Dictionary<string, string> formValues;
-
-		internal Form(Browser browser, CQ formNode, Uri baseUri)
-		{
-			this.browser = browser;
-			this.formNode = formNode;
-			this.baseUri = baseUri;
-
-			if (!formNode.Is("form"))
-			{
-				throw new ArgumentException("Selected node must be of type <form />");
-			}
-
-			formValues = new Dictionary<string, string>();
-
-
-				formNode
-				.Select("input[type=\"text\"]," +
-						  "input[type=\"hidden\"]," +
-						  "input[type=\"submit\"]," +
-						  "input[type=\"radio\"]:checked," +
-						  "input[type=\"checkbox\"]:checked," +
-				        "select," +
-				        "input:not([type])," +
-				        "textarea",formNode)
-				.Where(x => !string.IsNullOrWhiteSpace(x.Name))
-				.Select(x =>
-					new KeyValuePair<string, string>(x.Name, x.NodeName == "TEXTAREA" ? x.InnerHTML : x.Value)
-					).ForEach(x => formValues[x.Key] = x.Value);
-		}
-
-		public Form Set(object values)
-		{
-			foreach (var propertyInfo in values.GetType().GetProperties())
-			{
-				formValues[propertyInfo.Name] = propertyInfo.GetValue(values).ToString();
-			}
-			return this;
-		}
-
-		public Form Set(string inputName, string value)
-		{
-			formValues[inputName]= value;
-			return this;
-		}
-
-		public string this[string key]
-		{
-			get { return formValues[key]; }
-			set { formValues[key] = value; }
-		}
-
-
-		public Task<WebPage> Submit(bool asJson = false)
-		{
-			var formurl = formNode.Attr("action") ?? "";
-			var uri = new Uri(baseUri, formurl);
-
-			return browser.OpenWithData(uri.ToString(), Method, formValues, asJson);
-		}
-
-		public HttpVerb Method
-		{
-			get
-			{
-				return String.Compare("post", formNode.Attr("method") ?? "", StringComparison.OrdinalIgnoreCase) == 0
-					? HttpVerb.Post
-					: HttpVerb.Get;
-			}
-			set { formNode.Attr("method", value.ToString()); }
-		}
-	}
-}
+﻿using System;using System.Collections.Generic;using System.Linq;using System.Threading.Tasks;using CsQuery;using CsQuery.ExtensionMethods;namespace Scrappy{	public class Form	{		private readonly Browser browser;		private readonly CQ formNode;		private readonly Uri baseUri;		private readonly Dictionary<string, string> formValues;		internal Form(Browser browser, CQ formNode, Uri baseUri)		{			this.browser = browser;			this.formNode = formNode;			this.baseUri = baseUri;			if (!formNode.Is("form"))			{				throw new ArgumentException("Selected node must be of type <form />");			}			formValues = new Dictionary<string, string>();			formNode			.Select("input[type=\"text\"]," +					  "input[type=\"hidden\"]," +					  "input[type=\"submit\"]," +					  "input[type=\"radio\"]:checked," +					  "input[type=\"checkbox\"]:checked," +					  "select," +					  "input:not([type])," +					  "textarea", formNode)			.Where(x => !string.IsNullOrWhiteSpace(x.Name))			.Select(x => new				{					Key = x.Name, 					Value= x.NodeName == "TEXTAREA" ? x.InnerHTML : x.Value				}).ForEach(x => formValues[x.Key] = x.Value);		}		public Form Set(object values)		{			foreach (var propertyInfo in values.GetType().GetProperties())			{				formValues[propertyInfo.Name] = propertyInfo.GetValue(values).ToString();			}			return this;		}		public Form Set(string inputName, string value)		{			formValues[inputName] = value;			return this;		}		public string this[string key]		{			get { return formValues[key]; }			set { formValues[key] = value; }		}		public Task<WebPage> Submit(bool asJson = false)		{			var formurl = formNode.Attr("action") ?? "";			var uri = new Uri(baseUri, formurl);			return browser.OpenWithFormData(uri.ToString(), Method, formValues, asJson);		}		public HttpVerb Method		{			get			{				return String.Compare("post", formNode.Attr("method") ?? "", StringComparison.OrdinalIgnoreCase) == 0					? HttpVerb.Post					: HttpVerb.Get;			}			set { formNode.Attr("method", value.ToString()); }		}	}}
